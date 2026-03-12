7f6aca5c8b0eeb322e9205f3c9ec33e506938f21
Rafael J <rafa061167@Gmail.com>
2026-03-12 09:57:39 -0400

    cambios

--- Jose.py
+++ Jose.py
@@ -7,3 +7,10 @@ lo que significa que está diseñado para ser utilizado
 en una serie de aplicaciones, incluyendo ciencias de datos, 
 desarrollo de software_version y web, automatización y, 
 en general, para hacer cosas
+
+"Escribir programas es una actividad muy creativa y gratificante", 
+dice el instructor de la Universidad de Michigan y Coursera, 
+Charles R Severance, en su libro Python for Everybody. 
+"Puedes escribir programas por muchas razones, desde ganarte 
+la vida hasta resolver un problema difícil de análisis de datos, 
+pasando por divertirte o ayudar a otra persona a resolver un problema."
